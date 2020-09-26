e0f9e679d3896600ec416378657254e6475a8467
Kamal <kamalnature772@gmail.com>
2020-09-26 21:26:09 +0545

    Added new file

---
+++ LINQ/Country.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+public class Country
+{
+    public string Name{ get; set;}
+    public string Continent{ get; set;}
+    public double Area{ get; set;}
+    public long Population{ get; set;}
+    public DateTime IndependenceDay{ get; set;}
+
+    public Country(string name, string continent, double area, long population, DateTime independenceDay = default )
+    {
+        Name = name;
+        Continent = continent;
+        Area = area;
+        Population = population;
+        IndependenceDay = independenceDay;
+    }
+    public static List<Country> GetCountries()
+    {
+        Country c1 = new Country("Nepal", "Asia", 147181, 65244221);
+        Country c2 = new Country("India", "Asia", 5456872, 5488415);
+        Country c3 = new Country("Bangladesh", "Asia", 12542.22, 12541);
+        Country c4 = new Country("Portugal", "Europe", 12042.251, 12540, "2008,04,14");
+        Country c5 = new Country("China", "Asia", 98745.558, 278451);
+        Country c6 = new Country("Spain", "Europe", 25045, 62510);
+        Country c7 = new Country("Romania", "Europe", 21250.241, 26250);
+        Country c8 = new Country("france", "Europe", 564781, 2350420);
+        Country c9 = new Country("Sweden", "Europe", 987466,789446);
+        Country c10 = new Country("Argentina", "South America", 225522, 246324520);
+        Country c11 = new Country("Brazil", "South America", 254006.30, 504600);
+        Country c12 = new Country("USA", "North America", 802506.30, 600454);
+        Country c13 = new Country("canada", "North America", 504.2205, 604550);
+        Country c14 = new Country("Afghanistan", "Asia", 505210, 50420);
+        Country c15 = new Country("Egypt", "Africa", 604840.50, 60450);
+        Country c16 = new Country("Congo", "Africa", 789410.2,12420);
+        Country c17 = new Country("Cuba", "North America", 22502.651, 89781);
+        Country c18 = new Country("Mexico", "North America", 6578.875, 269715);
+        Country c19 = new Country("Columbia", "South America", 7810.2, 2985150);
+        Country c20 = new Country("Peru", "South America", 12540.20, 7895);
+        Country c21 = new Country("Australia", "Australia", 78915.055, 89745);
+        Country c22 = new Country("Fiji", "Australia", 78050.50, 25151);
+        Country c23 = new Country("Solomon Islands", "Australia", 840404.202, 324520);
+        Country c24 = new Country("Germany", "Europe", 815960.30, 24620);
+        Country c25 = new Country("Ireland", "Europe", 22552.5012, 24630);
+        var countries = new List<Country>() {c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15,
+                                                c16, c17, c18, c19, c20, c12, c22, c23, c24, c25};
+
+        return countries;
+    }
+
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // LINQ  Language Integrated Query
+            // LINQ to objects
+
+            int [] numbers = { 23, 55, 12, 45, 65, 78, 20, 45, 30, 77};
+
+            // Fetch numbers which is greater than 50
+
+            // Using method syntax -   lambda expression
+            var numbersGreaterThan50 = numbers.Where(x => x > 50);
+
+            // Using query syntax  - Query expression
+            var numbersGreaterThanFifty =   from num in numbers
+                                            where num > 50
+                                            select num;
+
+            string[] names = {"Kamal", "Ranjit", "Roshan", "Ashish", "Rb", "Cosmic", "Paras", "Rajesh"};
+
+            var namesStartingWithR = names.Where(x => x.ToLower().StartsWith("R") && x.Length > 3);
+
+            var namesStartsWithR =  from name in names
+                                    where name.ToLower().StartsWith("R") && name.Length > 3
+                                    select name;
+
+            foreach(var name in namesStartingWithR)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            List<Country> countries = new List<Country>();
+            /*Country c1 = new Country();
+            c1.Name = "Nepal";
+            c1.Continent = "Asia";
+            c1.Area = 147181;
+            c1.Polulation = 26494504;
+
+            countries.Add(c1);
+            Country c2 = new Country() {Name = "India", Continent = "Asia", Area = 5045155.245, Polulation = 5453552.2355};
+            */
+            var countries1 = Country.GetCountries();
+
+            var asianCountries = from country in countries1
+                                 where country.Continent == "Asia"
+                                 select country.Name;
+            
+            foreach(var country in asianCountries)
+            {
+                Console.WriteLine(country);
+            }
+
+            
+            var countriesWithMoreThan100kPopulation = from population in countries1
+                                      where population.Population < 100000
+                                      select population.Name;
+                                      
+                                      
+
+            foreach(var country in countriesWithMoreThan100kPopulation )
+            {
+                Console.WriteLine(country);
+            }
+
+
+
+
+
+                                   
+        }
+    }
+}
